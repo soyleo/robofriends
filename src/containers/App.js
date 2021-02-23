f378ce9c9ce1e8d4d616ae1d58037dcc10728d86
soyleo <leopoldo230791@gmail.com>
2021-02-23 12:00:58 -0300

    folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import CardList from './CardList';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends React.Component {
     constructor(){

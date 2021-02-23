4ab0b516e18d0956048eab2bc08b190c6e0cecd0
soyleo <leopoldo230791@gmail.com>
2021-02-23 12:34:00 -0300

    build

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,23 +27,22 @@ class App extends React.Component {
 
     render()
     {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
-        if(this.state.robots.length === 0){
-            return <h1 className='f1 tc'>Loading</h1>
-        } else {
-            return(
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-                
+        return !robots.length?
+            <h1 className='f1 tc'>Loading</h1> :
+            (
+                    <div className='tc'>
+                        <h1 className='f1'>RoboFriends</h1>
+                        <SearchBox searchChange={this.onSearchChange}/>
+                        <Scroll>
+                            <CardList robots={filteredRobots} />
+                        </Scroll>
+                    </div>
+                    
             );
-        }
     }
 }
     

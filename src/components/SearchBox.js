dce45b2f7a0fb2fd442bd6d0c12e2c818a7bf553
soyleo <leopoldo230791@gmail.com>
2021-02-24 00:41:41 -0300

    Restyle of searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,7 @@ import './SearchBox.css'
 const SearchBox = ({ searchfield, searchChange}) =>{
     return (
         <div className='pa2 tc'>
-            <input className='pa3 ba b--hot-pink bg-lightest-pink br3' type='search' placeholder='Search Robots' onChange={searchChange} />
+            <input className='tc pa3 ba b--hot-pink bg-lightest-pink br3 w-50' type='search' placeholder='Search Robots' onChange={searchChange} />
         </div>
     );
 }
